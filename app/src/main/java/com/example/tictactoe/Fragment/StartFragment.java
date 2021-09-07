3c77afa26a42b80bb6292c32ca9d46cdee916f3b
AlexLeica <alex_k1_leica@yahoo.com>
2021-09-07 20:22:26 +0300

    Tictactoeapp

---
+++ app/src/main/java/com/example/tictactoe/Fragment/StartFragment.java
@@ -0,0 +1,59 @@
+package com.example.tictactoe.Fragment;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+
+import com.example.tictactoe.MainActivity;
+import com.example.tictactoe.R;
+
+public class StartFragment extends Fragment {
+
+    private Button r_play_with_computer, r_play_2_player;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+        r_play_with_computer = view.findViewById(R.id.r_play_with_computer);
+        r_play_2_player = view.findViewById(R.id.r_play_2_player);
+
+
+        r_play_with_computer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.multiPlayer = false;
+                MainActivity.scoreO = 0;
+                MainActivity.scoreX = 0;
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.addToBackStack(GameFragment.TAG);
+                transaction.replace(R.id.frame_main, new GameFragment());
+                transaction.commit();
+            }
+        });
+        r_play_2_player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.multiPlayer = true;
+                MainActivity.scoreO = 0;
+                MainActivity.scoreX = 0;
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.addToBackStack(GameFragment.TAG);
+                transaction.replace(R.id.frame_main, new GameFragment());
+                transaction.commit();
+            }
+        });
+
+        return view;
+    }
+}
